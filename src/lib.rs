5a9b0ecb69f449ff6ef732edf531f803ed3eddd9
Akishika Issui <kshkss@users.noreply.github.com>
2021-09-03 07:55:02 +0900

    Add a function to call lsode with user specific jacobian

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use libc::{c_double, c_int};
 use std::slice;
 use libffi::high::Closure4;
+use libffi::high::Closure7;
 
 
 #[link(name = "gfortran")]
@@ -23,20 +24,20 @@ extern {
                lrw: &c_int,
                iwork: *mut c_int,
                liw: &c_int,
-               jac: extern fn(&c_int, &c_double, *mut c_double, &c_int, &c_int, *const c_double, &c_int),
+               jac: extern fn(*const c_int, *const c_double, *const c_double, *const c_int, *const c_int, *mut c_double, *const c_int),
                mf: &c_int
                );
 }
 
 /// A dummy function to pass to `dlsode_` in case the user does not want to specify a Jacobian.
 pub extern fn fake_jacobian(
-    _neq: &c_int,
-    _t:   &c_double,
-    _y:   *mut c_double,
-    _ml:  &c_int,
-    _mu:  &c_int,
-    _pd:  *const c_double,
-    _nr:  &c_int
+    _neq: *const c_int,
+    _t:   *const c_double,
+    _y:   *const c_double,
+    _ml:  *const c_int,
+    _mu:  *const c_int,
+    _pd:  *mut c_double,
+    _nr:  *const c_int
     ) { }
 
 
@@ -125,6 +126,100 @@ where F: Fn(&[f64], &f64) -> Vec<f64>
                 iwork.as_mut_ptr(),
                 &(liw as i32),
                 fake_jacobian,
+                &mf
+                ); }
+
+        result.push(y.clone());
+    }
+    result
+}
+
+use ndarray::prelude::*;
+
+pub fn solve_ode_with_jacobian<F, G>(
+    rhs: F, 
+    jacobian: G,
+    y0: &[f64],
+    t_dense: Vec<f64>,
+    atol: f64,
+    rtol: f64,
+    ) -> Vec<Vec<f64>> 
+where
+    F: Fn(&[f64], &f64) -> Vec<f64>,
+    G: Fn(&[f64], &f64) -> Array2<f64>,
+{
+
+    let f = | n: *const c_int, t_ptr: *const c_double, y_ptr: *mut c_double, dy_ptr: *mut c_double | {
+        let (dy, y, t) = unsafe {
+            (
+                slice::from_raw_parts_mut(dy_ptr, n as usize),
+                slice::from_raw_parts(y_ptr, n as usize),
+                *t_ptr
+            )
+        };
+        let dy_new = rhs(y, &t);
+        for (i, deriv) in dy_new.iter().enumerate() {
+            dy[i] = *deriv
+        }
+    };
+    let closure = Closure4::new(&f);
+    let call = closure.code_ptr();
+
+    let n = y0.len();
+    let g = | _n: *const c_int, t_ptr: *const c_double, y_ptr: *const c_double, _ml: *const c_int, _mu: *const c_int, dy_ptr: *mut c_double, _nrow: *const c_int | {
+        debug_assert!( unsafe{ n as i32 == *_n } );
+        let (dy, y, t) = unsafe {
+            (
+                slice::from_raw_parts_mut(dy_ptr, n as usize * n as usize),
+                slice::from_raw_parts(y_ptr, n as usize),
+                *t_ptr
+            )
+        };
+        let mut dy = ArrayViewMut1::<f64>::from(dy);
+        let dy_new = jacobian(y, &t);
+        for (dest, jac) in dy.iter_mut().zip(dy_new.t().iter()) {
+            *dest = *jac;
+        }
+    };
+
+    let jacobian_closure = Closure7::new(&g);
+    let call_jacobian = jacobian_closure.code_ptr();
+
+    let mut y: Vec<f64> = y0.to_vec();
+    let n = y0.len();
+    let mut t = t_dense[0];
+
+    let itol = 1;
+    let itask = 1;
+    let iopt = 0;
+    let mut istate = 1;
+    let mf = 21; // Stiff (BDF) method, user-supplied full Jacobian.
+
+    let lrw = 22 + 9*n + n*n;
+    let liw = 20 + n;
+    let mut rwork: Vec<f64> = (0..lrw).map(|_i| 0.0 as c_double).collect();
+    let mut iwork: Vec<i32> = (0..liw).map(|_i| 0 as c_int).collect();
+
+    let mut result = Vec::new();
+
+    for tout in t_dense {
+        unsafe { dlsode_(
+                *call,
+                &(n as i32),
+                y.as_mut_ptr(),
+                &mut t,
+                &tout,
+                &itol,
+                &rtol,
+                &atol,
+                &itask,
+                &mut istate,
+                &iopt,
+                rwork.as_mut_ptr(),
+                &(lrw as i32),
+                iwork.as_mut_ptr(),
+                &(liw as i32),
+                *call_jacobian,
                 &mf
                 ); }
 
