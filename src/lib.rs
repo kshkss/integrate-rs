6ffca057d1d5dcdf33be2197608f8d004e030e7d
Akishika Issui <kshkss@users.noreply.github.com>
2021-09-05 14:52:51 +0900

    Format codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,58 @@
 use libc::{c_double, c_int};
-use std::slice;
 use libffi::high::Closure4;
 use libffi::high::Closure7;
-
+use std::slice;
 
 #[link(name = "gfortran")]
-extern {
+extern "C" {
     /// Call `DLSODE` subroutine from ODEPACK
     ///
     /// For info on passed arguments look inside ODEPACK.
-    pub fn dlsode_(f: extern fn(*const c_int, *const c_double, *mut c_double, *mut c_double),
-               neq: &c_int,
-               y: *mut c_double,
-               t: &mut c_double,
-               tout: &c_double,
-               itol: &c_int,
-               rtol: &c_double,
-               atol: &c_double,
-               itask: &c_int,
-               istate: &mut c_int,
-               iopt: &c_int,
-               rwork: *mut c_double,
-               lrw: &c_int,
-               iwork: *mut c_int,
-               liw: &c_int,
-               jac: extern fn(*const c_int, *const c_double, *const c_double, *const c_int, *const c_int, *mut c_double, *const c_int),
-               mf: &c_int
-               );
+    pub fn dlsode_(
+        f: extern "C" fn(*const c_int, *const c_double, *mut c_double, *mut c_double),
+        neq: &c_int,
+        y: *mut c_double,
+        t: &mut c_double,
+        tout: &c_double,
+        itol: &c_int,
+        rtol: &c_double,
+        atol: &c_double,
+        itask: &c_int,
+        istate: &mut c_int,
+        iopt: &c_int,
+        rwork: *mut c_double,
+        lrw: &c_int,
+        iwork: *mut c_int,
+        liw: &c_int,
+        jac: extern "C" fn(
+            *const c_int,
+            *const c_double,
+            *const c_double,
+            *const c_int,
+            *const c_int,
+            *mut c_double,
+            *const c_int,
+        ),
+        mf: &c_int,
+    );
 }
 
 /// A dummy function to pass to `dlsode_` in case the user does not want to specify a Jacobian.
-pub extern fn fake_jacobian(
+pub extern "C" fn fake_jacobian(
     _neq: *const c_int,
-    _t:   *const c_double,
-    _y:   *const c_double,
-    _ml:  *const c_int,
-    _mu:  *const c_int,
-    _pd:  *mut c_double,
-    _nr:  *const c_int
-    ) { }
+    _t: *const c_double,
+    _y: *const c_double,
+    _ml: *const c_int,
+    _mu: *const c_int,
+    _pd: *mut c_double,
+    _nr: *const c_int,
+) {
+}
 
-use std::sync::Mutex;
 use lazy_static::lazy_static;
+use std::sync::Mutex;
 
-lazy_static!{
+lazy_static! {
     static ref flag: Mutex<()> = Mutex::<()>::new(());
 }
 
@@ -51,9 +60,9 @@ lazy_static!{
 /// First time in `t_dense` has to be the initial time.
 ///
 /// Each equation in the system of ODEs has the form:
-/// 
+///
 /// > *dy/dt = f(y, t)*
-/// 
+///
 /// The function expects the function *f* as the first argument `rhs`.
 /// Initial state is given in `y0`.
 ///
@@ -64,36 +73,31 @@ lazy_static!{
 /// let ts = vec![0.0, 1.0];
 /// let f = |y: &[f64], t: &f64| {
 ///     let mut dy = vec![0.0];
-///     dy[0] = *t * y[0]; 
+///     dy[0] = *t * y[0];
 ///     dy
 ///     };
 /// let sol = lsode::solve_ode(f, &y0, ts, 1e-6, 1e-6);
-/// 
+///
 /// assert!((sol[1][0] - y0[0]*0.5_f64.exp()).abs() < 1e-3, "error too large");
 /// ```
-pub fn solve_ode<F>(
-    rhs: F, 
-    y0: &[f64],
-    t_dense: Vec<f64>,
-    atol: f64,
-    rtol: f64,
-    ) -> Vec<Vec<f64>> 
-where F: Fn(&[f64], &f64) -> Vec<f64>
+pub fn solve_ode<F>(rhs: F, y0: &[f64], t_dense: Vec<f64>, atol: f64, rtol: f64) -> Vec<Vec<f64>>
+where
+    F: Fn(&[f64], &f64) -> Vec<f64>,
 {
-
-    let f = | n: *const c_int, t_ptr: *const c_double, y_ptr: *mut c_double, dy_ptr: *mut c_double | {
-        let (dy, y, t) = unsafe {
-            (
-                slice::from_raw_parts_mut(dy_ptr, *n as usize),
-                slice::from_raw_parts(y_ptr, *n as usize),
-                *t_ptr
-            )
+    let f =
+        |n: *const c_int, t_ptr: *const c_double, y_ptr: *mut c_double, dy_ptr: *mut c_double| {
+            let (dy, y, t) = unsafe {
+                (
+                    slice::from_raw_parts_mut(dy_ptr, *n as usize),
+                    slice::from_raw_parts(y_ptr, *n as usize),
+                    *t_ptr,
+                )
+            };
+            let dy_new = rhs(y, &t);
+            for (i, deriv) in dy_new.iter().enumerate() {
+                dy[i] = *deriv
+            }
         };
-        let dy_new = rhs(y, &t);
-        for (i, deriv) in dy_new.iter().enumerate() {
-            dy[i] = *deriv
-        }
-    };
     let closure = Closure4::new(&f);
     let call = closure.code_ptr();
 
@@ -107,7 +111,7 @@ where F: Fn(&[f64], &f64) -> Vec<f64>
     let mut istate = 1;
     let mf = 22;
 
-    let lrw = 22 + 9*n + n*n;
+    let lrw = 22 + 9 * n + n * n;
     let liw = 20 + n;
     let mut rwork: Vec<f64> = (0..lrw).map(|_i| 0.0 as c_double).collect();
     let mut iwork: Vec<i32> = (0..liw).map(|_i| 0 as c_int).collect();
@@ -116,7 +120,8 @@ where F: Fn(&[f64], &f64) -> Vec<f64>
 
     let _lock = flag.lock().unwrap();
     for tout in t_dense {
-        unsafe { dlsode_(
+        unsafe {
+            dlsode_(
                 *call,
                 &(n as i32),
                 y.as_mut_ptr(),
@@ -133,8 +138,9 @@ where F: Fn(&[f64], &f64) -> Vec<f64>
                 iwork.as_mut_ptr(),
                 &(liw as i32),
                 fake_jacobian,
-                &mf
-                ); }
+                &mf,
+            );
+        }
 
         result.push(y.clone());
     }
@@ -144,40 +150,46 @@ where F: Fn(&[f64], &f64) -> Vec<f64>
 use ndarray::prelude::*;
 
 pub fn solve_ode_with_jacobian<F, G>(
-    rhs: F, 
+    rhs: F,
     jacobian: G,
     y0: &[f64],
     t_dense: Vec<f64>,
     atol: f64,
     rtol: f64,
-    ) -> Vec<Vec<f64>> 
+) -> Vec<Vec<f64>>
 where
     F: Fn(&[f64], &f64) -> Vec<f64>,
     G: Fn(&[f64], &f64) -> Array2<f64>,
 {
-
-    let f = | n: *const c_int, t_ptr: *const c_double, y_ptr: *mut c_double, dy_ptr: *mut c_double | {
-        let (dy, y, t) = unsafe {
-            (
-                slice::from_raw_parts_mut(dy_ptr, *n as usize),
-                slice::from_raw_parts(y_ptr, *n as usize),
-                *t_ptr
-            )
+    let f =
+        |n: *const c_int, t_ptr: *const c_double, y_ptr: *mut c_double, dy_ptr: *mut c_double| {
+            let (dy, y, t) = unsafe {
+                (
+                    slice::from_raw_parts_mut(dy_ptr, *n as usize),
+                    slice::from_raw_parts(y_ptr, *n as usize),
+                    *t_ptr,
+                )
+            };
+            let dy_new = rhs(y, &t);
+            for (i, deriv) in dy_new.iter().enumerate() {
+                dy[i] = *deriv
+            }
         };
-        let dy_new = rhs(y, &t);
-        for (i, deriv) in dy_new.iter().enumerate() {
-            dy[i] = *deriv
-        }
-    };
     let closure = Closure4::new(&f);
     let call = closure.code_ptr();
 
-    let g = | n: *const c_int, t_ptr: *const c_double, y_ptr: *const c_double, _ml: *const c_int, _mu: *const c_int, dy_ptr: *mut c_double, _nrow: *const c_int | {
+    let g = |n: *const c_int,
+             t_ptr: *const c_double,
+             y_ptr: *const c_double,
+             _ml: *const c_int,
+             _mu: *const c_int,
+             dy_ptr: *mut c_double,
+             _nrow: *const c_int| {
         let (dy, y, t) = unsafe {
             (
                 slice::from_raw_parts_mut(dy_ptr, *n as usize * *n as usize),
                 slice::from_raw_parts(y_ptr, *n as usize),
-                *t_ptr
+                *t_ptr,
             )
         };
         let mut dy = ArrayViewMut1::<f64>::from(dy);
@@ -200,7 +212,7 @@ where
     let mut istate = 1;
     let mf = 21; // Stiff (BDF) method, user-supplied full Jacobian.
 
-    let lrw = 22 + 9*n + n*n;
+    let lrw = 22 + 9 * n + n * n;
     let liw = 20 + n;
     let mut rwork: Vec<f64> = (0..lrw).map(|_i| 0.0 as c_double).collect();
     let mut iwork: Vec<i32> = (0..liw).map(|_i| 0 as c_int).collect();
@@ -208,7 +220,8 @@ where
     let mut result = Vec::new();
 
     for tout in t_dense {
-        unsafe { dlsode_(
+        unsafe {
+            dlsode_(
                 *call,
                 &(n as i32),
                 y.as_mut_ptr(),
@@ -225,8 +238,9 @@ where
                 iwork.as_mut_ptr(),
                 &(liw as i32),
                 *call_jacobian,
-                &mf
-                ); }
+                &mf,
+            );
+        }
 
         result.push(y.clone());
     }
@@ -241,5 +255,7 @@ where
 /// assert_eq!(lsode::linspace(0.0, 2.0, 3), vec![0.0, 1.0, 2.0]);
 /// ```
 pub fn linspace(start: f64, stop: f64, n_points: usize) -> Vec<f64> {
-    (0..n_points).map(|i| start + i as f64*(stop - start)/(n_points - 1) as f64).collect()
+    (0..n_points)
+        .map(|i| start + i as f64 * (stop - start) / (n_points - 1) as f64)
+        .collect()
 }
