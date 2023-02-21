ab0fa9569f46d6927c972228907ccc19a697ebd1
Akishika Issui <kshkss@users.noreply.github.com>
2023-02-22 01:39:06 +0900

    test(odepack): use new APIs in `pendulum.rs`

--- examples/pendulum.rs
+++ examples/pendulum.rs
@@ -1,4 +1,4 @@
-use integrate::BDF;
+use integrate::odepack::Lsode;
 use ndarray::prelude::*;
 
 /// This example has the setup from:
@@ -9,18 +9,14 @@ fn main() {
     let g: f64 = 9.81;
     let torque = |t: f64| 0.1 * t.sin();
 
-    let f = |y: &[f64], t: f64| {
-        let mut dy = vec![0.0, 0.0];
+    let f = |t: f64, y: &[f64], dy: &mut [f64]| {
         dy[0] = y[1];
         dy[1] = -3.0 * g / (2.0 * l) * y[0].sin() + 3.0 / (m * l * l) * torque(t);
-        dy
     };
 
     let y0 = [0.01, 0.0];
     let ts = Array1::linspace(0.0, 10.0, 1000).to_vec();
-    let (atol, rtol) = (1e-6, 1e-6);
-
-    let sol = BDF::new(f).solve(&y0, &ts, atol, rtol);
+    let sol = Lsode::new(&f, Default::default()).run(&y0, &ts);
 
     for (i, t) in sol.iter().zip(ts) {
         println!("{} {} {}", t, i[0], i[1]);
