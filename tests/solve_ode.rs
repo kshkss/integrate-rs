910728e4e81a78ffaf38c5bb9a2a6b9d9992a762
Akishika Issui <kshkss@users.noreply.github.com>
2023-02-22 01:36:12 +0900

    test(odepack): use new api in `solve_ode.rs`

--- tests/solve_ode.rs
+++ tests/solve_ode.rs
@@ -1,3 +1,5 @@
+use integrate::odepack::Lsode;
+
 fn solution_stiff(t: f64) -> [f64; 2] {
     [
         2.0 * (-t).exp() - (-(1000.0 * t)).exp(),
@@ -5,21 +7,17 @@ fn solution_stiff(t: f64) -> [f64; 2] {
     ]
 }
 
-fn rhs_stiff(y: &[f64], _t: f64) -> Vec<f64> {
-    let mut dy = vec![0.0, 0.0];
+fn rhs_stiff(_t: f64, y: &[f64], dy: &mut [f64]) {
     dy[0] = 998.0 * y[0] + 1998.0 * y[1];
     dy[1] = -999.0 * y[0] - 1999.0 * y[1];
-    dy
 }
 
 #[test]
 fn stiff() {
     let y0 = [1.0, 0.0];
     let ts: Vec<f64> = (0..10).map(|i| 0.1 * i as f64).collect();
-    let atol = 1e-6;
-    let rtol = 1e-8;
 
-    let sol = integrate::BDF::new(rhs_stiff).solve(&y0, &ts, atol, rtol);
+    let sol = Lsode::new(&rhs_stiff, Default::default()).run(&ts, &y0);
 
     for (analytical, calculated) in ts.iter().map(|x| solution_stiff(*x)).zip(sol) {
         assert!(
@@ -41,15 +39,15 @@ use ndarray::prelude::*;
 
 #[test]
 fn stiff_with_jacobian() {
-    let g = |_y: &[f64], _t: f64| -> Array2<f64> { array![[998., 1998.], [-999., -1999.]] };
+    let g = |_t: f64, _y: &[f64], mut jac: ArrayViewMut2<f64>| {
+        jac.assign(&array![[998., 1998.], [-999., -1999.]])
+    };
     let y0 = [1.0, 0.0];
     let ts: Vec<f64> = (0..10).map(|i| 0.1 * i as f64).collect();
-    let atol = 1e-6;
-    let rtol = 1e-8;
 
-    let sol = integrate::BDF::new(rhs_stiff)
-        .gen_full_jacobian_by(g)
-        .solve(&y0, &ts, atol, rtol);
+    let sol = Lsode::new(&rhs_stiff, Default::default())
+        .gen_full_jacobian_by(&g)
+        .run(&ts, &y0);
 
     for (analytical, calculated) in ts.iter().map(|x| solution_stiff(*x)).zip(sol) {
         assert!(
@@ -71,20 +69,16 @@ fn solution_decay(t: f64) -> [f64; 1] {
     [1000.0 * (-t).exp()]
 }
 
-fn rhs_decay(y: &[f64], _t: f64) -> Vec<f64> {
-    let mut dy = vec![0.0];
+fn rhs_decay(_t: f64, y: &[f64], dy: &mut [f64]) {
     dy[0] = -y[0];
-    dy
 }
 
 #[test]
 fn decay() {
     let y0 = [1000.0];
     let ts: Vec<f64> = (0..7).map(|i| 1.0 * i as f64).collect();
-    let atol = 1e-6;
-    let rtol = 1e-8;
 
-    let sol = integrate::BDF::new(rhs_decay).solve(&y0, &ts, atol, rtol);
+    let sol = Lsode::new(&rhs_decay, Default::default()).run(&ts, &y0);
 
     println!("{:?}", sol);
 
@@ -101,14 +95,14 @@ fn decay() {
 #[test]
 fn decay_with_jacobian() {
     let y0 = [1000.0];
-    let g = |_y: &[f64], _t: f64| -> Array2<f64> { array![[-1.]] };
+    let g = |_t: f64, _y: &[f64], mut jac: ArrayViewMut2<f64>| {
+        jac[[0, 0]] = -1.;
+    };
     let ts: Vec<f64> = (0..7).map(|i| 1.0 * i as f64).collect();
-    let atol = 1e-6;
-    let rtol = 1e-8;
 
-    let sol = integrate::BDF::new(rhs_decay)
-        .gen_full_jacobian_by(g)
-        .solve(&y0, &ts, atol, rtol);
+    let sol = Lsode::new(&rhs_decay, Default::default())
+        .gen_full_jacobian_by(&g)
+        .run(&ts, &y0);
 
     println!("{:?}", sol);
 
@@ -126,12 +120,10 @@ fn decay_with_jacobian() {
 fn closure_rhs() {
     let y0 = [1.0];
     let ts = vec![0.0, 1.0];
-    let f = |y: &[f64], t: f64| {
-        let mut dy = vec![0.0];
+    let f = |t: f64, y: &[f64], dy: &mut [f64]| {
         dy[0] = t * y[0];
-        dy
     };
-    let sol = integrate::BDF::new(f).solve(&y0, &ts, 1e-6, 1e-6);
+    let sol = Lsode::new(&f, Default::default()).run(&ts, &y0);
     println!("{:?}", sol);
     assert!(
         (sol[1][0] - y0[0] * 0.5_f64.exp()).abs() < 1e-3,
@@ -143,15 +135,13 @@ fn closure_rhs() {
 fn closure_rhs_with_jacobian() {
     let y0 = [1.0];
     let ts = vec![0.0, 1.0];
-    let f = |y: &[f64], t: f64| {
-        let mut dy = vec![0.0];
+    let f = |t: f64, y: &[f64], dy: &mut [f64]| {
         dy[0] = t * y[0];
-        dy
     };
-    let g = |_y: &[f64], t: f64| array![[t]];
-    let sol = integrate::BDF::new(f)
-        .gen_full_jacobian_by(g)
-        .solve(&y0, &ts, 1e-6, 1e-6);
+    let g = |t: f64, _y: &[f64], mut dy: ArrayViewMut2<f64>| dy[[0, 0]] = t;
+    let sol = Lsode::new(&f, Default::default())
+        .gen_full_jacobian_by(&g)
+        .run(&ts, &y0);
     println!("{:?}", sol);
     assert!(
         (sol[1][0] - y0[0] * 0.5_f64.exp()).abs() < 1e-3,
@@ -163,10 +153,13 @@ fn closure_rhs_with_jacobian() {
 fn closure_rhs_estimate_banded_jacobian() {
     let y0 = [1.0, 10.];
     let ts = vec![0.0, 1.0];
-    let f = |y: &[f64], t: f64| vec![t * y[0], (t + 2.) * y[1]];
-    let sol = integrate::BDF::new(f)
+    let f = |t: f64, y: &[f64], dy: &mut [f64]| {
+        dy[0] = t * y[0];
+        dy[1] = (t + 2.) * y[1];
+    };
+    let sol = Lsode::new(&f, Default::default())
         .gen_banded_jacobian(0, 0)
-        .solve(&y0, &ts, 1e-6, 1e-6);
+        .run(&ts, &y0);
     println!("{:?}", sol);
     assert!(
         (sol[1][0] - y0[0] * 0.5_f64.exp()).abs() < 1e-3,
