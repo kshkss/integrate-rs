ef8f8bb99664c377925c7335efbfce881ea31469
Akishika Issui <kshkss@users.noreply.github.com>
2023-03-04 17:07:52 +0900

    feat(radau): add an interface call fortran code

--- build.rs
+++ build.rs
@@ -8,13 +8,24 @@ fn main() {
             .flag("-fallow-argument-mismatch")
             .file("src/odepack.f")
             .compile("libodepack.a");
+        cc::Build::new()
+            .flag("-w")
+            // use for version of gfortran 10+
+            .flag("-fallow-argument-mismatch")
+            .file("src/radau.f")
+            .compile("libradau.a");
     } else {
         cc::Build::new()
             .flag("-w")
             .file("src/odepack.f")
             .compile("libodepack.a");
+        cc::Build::new()
+            .flag("-w")
+            .file("src/radau.f")
+            .compile("libradau.a");
     }
 
     println!("cargo:rustc-link-lib=static=odepack");
+    println!("cargo:rustc-link-lib=static=radau");
     println!("cargo:rustc-link-lib=dylib=gfortran");
 }
