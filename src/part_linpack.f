78cdac9b2aae5fd39a3be40e806dad5f1c0051c3
Akishika Issui <kshkss@users.noreply.github.com>
2023-04-19 00:59:32 +0900

    chore(odepack): split fortran code

---
+++ src/part_linpack.f
@@ -0,0 +1,580 @@
+*DECK DGEFA
+      SUBROUTINE DGEFA (A, LDA, N, IPVT, INFO)
+C***BEGIN PROLOGUE  DGEFA
+C***PURPOSE  Factor a matrix using Gaussian elimination.
+C***CATEGORY  D2A1
+C***TYPE      DOUBLE PRECISION (SGEFA-S, DGEFA-D, CGEFA-C)
+C***KEYWORDS  GENERAL MATRIX, LINEAR ALGEBRA, LINPACK,
+C             MATRIX FACTORIZATION
+C***AUTHOR  Moler, C. B., (U. of New Mexico)
+C***DESCRIPTION
+C
+C     DGEFA factors a double precision matrix by Gaussian elimination.
+C
+C     DGEFA is usually called by DGECO, but it can be called
+C     directly with a saving in time if  RCOND  is not needed.
+C     (Time for DGECO) = (1 + 9/N)*(Time for DGEFA) .
+C
+C     On Entry
+C
+C        A       DOUBLE PRECISION(LDA, N)
+C                the matrix to be factored.
+C
+C        LDA     INTEGER
+C                the leading dimension of the array  A .
+C
+C        N       INTEGER
+C                the order of the matrix  A .
+C
+C     On Return
+C
+C        A       an upper triangular matrix and the multipliers
+C                which were used to obtain it.
+C                The factorization can be written  A = L*U  where
+C                L  is a product of permutation and unit lower
+C                triangular matrices and  U  is upper triangular.
+C
+C        IPVT    INTEGER(N)
+C                an integer vector of pivot indices.
+C
+C        INFO    INTEGER
+C                = 0  normal value.
+C                = K  if  U(K,K) .EQ. 0.0 .  This is not an error
+C                     condition for this subroutine, but it does
+C                     indicate that DGESL or DGEDI will divide by zero
+C                     if called.  Use  RCOND  in DGECO for a reliable
+C                     indication of singularity.
+C
+C***REFERENCES  J. J. Dongarra, J. R. Bunch, C. B. Moler, and G. W.
+C                 Stewart, LINPACK Users' Guide, SIAM, 1979.
+C***ROUTINES CALLED  DAXPY, DSCAL, IDAMAX
+C***REVISION HISTORY  (YYMMDD)
+C   780814  DATE WRITTEN
+C   890831  Modified array declarations.  (WRB)
+C   890831  REVISION DATE from Version 3.2
+C   891214  Prologue converted to Version 4.0 format.  (BAB)
+C   900326  Removed duplicate information from DESCRIPTION section.
+C           (WRB)
+C   920501  Reformatted the REFERENCES section.  (WRB)
+C***END PROLOGUE  DGEFA
+      INTEGER LDA,N,IPVT(*),INFO
+      DOUBLE PRECISION A(LDA,*)
+C
+      DOUBLE PRECISION T
+      INTEGER IDAMAX,J,K,KP1,L,NM1
+C
+C     GAUSSIAN ELIMINATION WITH PARTIAL PIVOTING
+C
+C***FIRST EXECUTABLE STATEMENT  DGEFA
+      INFO = 0
+      NM1 = N - 1
+      IF (NM1 .LT. 1) GO TO 70
+      DO 60 K = 1, NM1
+         KP1 = K + 1
+C
+C        FIND L = PIVOT INDEX
+C
+         L = IDAMAX(N-K+1,A(K,K),1) + K - 1
+         IPVT(K) = L
+C
+C        ZERO PIVOT IMPLIES THIS COLUMN ALREADY TRIANGULARIZED
+C
+         IF (A(L,K) .EQ. 0.0D0) GO TO 40
+C
+C           INTERCHANGE IF NECESSARY
+C
+            IF (L .EQ. K) GO TO 10
+               T = A(L,K)
+               A(L,K) = A(K,K)
+               A(K,K) = T
+   10       CONTINUE
+C
+C           COMPUTE MULTIPLIERS
+C
+            T = -1.0D0/A(K,K)
+            CALL DSCAL(N-K,T,A(K+1,K),1)
+C
+C           ROW ELIMINATION WITH COLUMN INDEXING
+C
+            DO 30 J = KP1, N
+               T = A(L,J)
+               IF (L .EQ. K) GO TO 20
+                  A(L,J) = A(K,J)
+                  A(K,J) = T
+   20          CONTINUE
+               CALL DAXPY(N-K,T,A(K+1,K),1,A(K+1,J),1)
+   30       CONTINUE
+         GO TO 50
+   40    CONTINUE
+            INFO = K
+   50    CONTINUE
+   60 CONTINUE
+   70 CONTINUE
+      IPVT(N) = N
+      IF (A(N,N) .EQ. 0.0D0) INFO = N
+      RETURN
+      END
+*DECK DGESL
+      SUBROUTINE DGESL (A, LDA, N, IPVT, B, JOB)
+C***BEGIN PROLOGUE  DGESL
+C***PURPOSE  Solve the real system A*X=B or TRANS(A)*X=B using the
+C            factors computed by DGECO or DGEFA.
+C***CATEGORY  D2A1
+C***TYPE      DOUBLE PRECISION (SGESL-S, DGESL-D, CGESL-C)
+C***KEYWORDS  LINEAR ALGEBRA, LINPACK, MATRIX, SOLVE
+C***AUTHOR  Moler, C. B., (U. of New Mexico)
+C***DESCRIPTION
+C
+C     DGESL solves the double precision system
+C     A * X = B  or  TRANS(A) * X = B
+C     using the factors computed by DGECO or DGEFA.
+C
+C     On Entry
+C
+C        A       DOUBLE PRECISION(LDA, N)
+C                the output from DGECO or DGEFA.
+C
+C        LDA     INTEGER
+C                the leading dimension of the array  A .
+C
+C        N       INTEGER
+C                the order of the matrix  A .
+C
+C        IPVT    INTEGER(N)
+C                the pivot vector from DGECO or DGEFA.
+C
+C        B       DOUBLE PRECISION(N)
+C                the right hand side vector.
+C
+C        JOB     INTEGER
+C                = 0         to solve  A*X = B ,
+C                = nonzero   to solve  TRANS(A)*X = B  where
+C                            TRANS(A)  is the transpose.
+C
+C     On Return
+C
+C        B       the solution vector  X .
+C
+C     Error Condition
+C
+C        A division by zero will occur if the input factor contains a
+C        zero on the diagonal.  Technically this indicates singularity
+C        but it is often caused by improper arguments or improper
+C        setting of LDA .  It will not occur if the subroutines are
+C        called correctly and if DGECO has set RCOND .GT. 0.0
+C        or DGEFA has set INFO .EQ. 0 .
+C
+C     To compute  INVERSE(A) * C  where  C  is a matrix
+C     with  P  columns
+C           CALL DGECO(A,LDA,N,IPVT,RCOND,Z)
+C           IF (RCOND is too small) GO TO ...
+C           DO 10 J = 1, P
+C              CALL DGESL(A,LDA,N,IPVT,C(1,J),0)
+C        10 CONTINUE
+C
+C***REFERENCES  J. J. Dongarra, J. R. Bunch, C. B. Moler, and G. W.
+C                 Stewart, LINPACK Users' Guide, SIAM, 1979.
+C***ROUTINES CALLED  DAXPY, DDOT
+C***REVISION HISTORY  (YYMMDD)
+C   780814  DATE WRITTEN
+C   890831  Modified array declarations.  (WRB)
+C   890831  REVISION DATE from Version 3.2
+C   891214  Prologue converted to Version 4.0 format.  (BAB)
+C   900326  Removed duplicate information from DESCRIPTION section.
+C           (WRB)
+C   920501  Reformatted the REFERENCES section.  (WRB)
+C***END PROLOGUE  DGESL
+      INTEGER LDA,N,IPVT(*),JOB
+      DOUBLE PRECISION A(LDA,*),B(*)
+C
+      DOUBLE PRECISION DDOT,T
+      INTEGER K,KB,L,NM1
+C***FIRST EXECUTABLE STATEMENT  DGESL
+      NM1 = N - 1
+      IF (JOB .NE. 0) GO TO 50
+C
+C        JOB = 0 , SOLVE  A * X = B
+C        FIRST SOLVE  L*Y = B
+C
+         IF (NM1 .LT. 1) GO TO 30
+         DO 20 K = 1, NM1
+            L = IPVT(K)
+            T = B(L)
+            IF (L .EQ. K) GO TO 10
+               B(L) = B(K)
+               B(K) = T
+   10       CONTINUE
+            CALL DAXPY(N-K,T,A(K+1,K),1,B(K+1),1)
+   20    CONTINUE
+   30    CONTINUE
+C
+C        NOW SOLVE  U*X = Y
+C
+         DO 40 KB = 1, N
+            K = N + 1 - KB
+            B(K) = B(K)/A(K,K)
+            T = -B(K)
+            CALL DAXPY(K-1,T,A(1,K),1,B(1),1)
+   40    CONTINUE
+      GO TO 100
+   50 CONTINUE
+C
+C        JOB = NONZERO, SOLVE  TRANS(A) * X = B
+C        FIRST SOLVE  TRANS(U)*Y = B
+C
+         DO 60 K = 1, N
+            T = DDOT(K-1,A(1,K),1,B(1),1)
+            B(K) = (B(K) - T)/A(K,K)
+   60    CONTINUE
+C
+C        NOW SOLVE TRANS(L)*X = Y
+C
+         IF (NM1 .LT. 1) GO TO 90
+         DO 80 KB = 1, NM1
+            K = N - KB
+            B(K) = B(K) + DDOT(N-K,A(K+1,K),1,B(K+1),1)
+            L = IPVT(K)
+            IF (L .EQ. K) GO TO 70
+               T = B(L)
+               B(L) = B(K)
+               B(K) = T
+   70       CONTINUE
+   80    CONTINUE
+   90    CONTINUE
+  100 CONTINUE
+      RETURN
+      END
+*DECK DGBFA
+      SUBROUTINE DGBFA (ABD, LDA, N, ML, MU, IPVT, INFO)
+C***BEGIN PROLOGUE  DGBFA
+C***PURPOSE  Factor a band matrix using Gaussian elimination.
+C***CATEGORY  D2A2
+C***TYPE      DOUBLE PRECISION (SGBFA-S, DGBFA-D, CGBFA-C)
+C***KEYWORDS  BANDED, LINEAR ALGEBRA, LINPACK, MATRIX FACTORIZATION
+C***AUTHOR  Moler, C. B., (U. of New Mexico)
+C***DESCRIPTION
+C
+C     DGBFA factors a double precision band matrix by elimination.
+C
+C     DGBFA is usually called by DGBCO, but it can be called
+C     directly with a saving in time if  RCOND  is not needed.
+C
+C     On Entry
+C
+C        ABD     DOUBLE PRECISION(LDA, N)
+C                contains the matrix in band storage.  The columns
+C                of the matrix are stored in the columns of  ABD  and
+C                the diagonals of the matrix are stored in rows
+C                ML+1 through 2*ML+MU+1 of  ABD .
+C                See the comments below for details.
+C
+C        LDA     INTEGER
+C                the leading dimension of the array  ABD .
+C                LDA must be .GE. 2*ML + MU + 1 .
+C
+C        N       INTEGER
+C                the order of the original matrix.
+C
+C        ML      INTEGER
+C                number of diagonals below the main diagonal.
+C                0 .LE. ML .LT.  N .
+C
+C        MU      INTEGER
+C                number of diagonals above the main diagonal.
+C                0 .LE. MU .LT.  N .
+C                More efficient if  ML .LE. MU .
+C     On Return
+C
+C        ABD     an upper triangular matrix in band storage and
+C                the multipliers which were used to obtain it.
+C                The factorization can be written  A = L*U  where
+C                L  is a product of permutation and unit lower
+C                triangular matrices and  U  is upper triangular.
+C
+C        IPVT    INTEGER(N)
+C                an integer vector of pivot indices.
+C
+C        INFO    INTEGER
+C                = 0  normal value.
+C                = K  if  U(K,K) .EQ. 0.0 .  This is not an error
+C                     condition for this subroutine, but it does
+C                     indicate that DGBSL will divide by zero if
+C                     called.  Use  RCOND  in DGBCO for a reliable
+C                     indication of singularity.
+C
+C     Band Storage
+C
+C           If  A  is a band matrix, the following program segment
+C           will set up the input.
+C
+C                   ML = (band width below the diagonal)
+C                   MU = (band width above the diagonal)
+C                   M = ML + MU + 1
+C                   DO 20 J = 1, N
+C                      I1 = MAX(1, J-MU)
+C                      I2 = MIN(N, J+ML)
+C                      DO 10 I = I1, I2
+C                         K = I - J + M
+C                         ABD(K,J) = A(I,J)
+C                10    CONTINUE
+C                20 CONTINUE
+C
+C           This uses rows  ML+1  through  2*ML+MU+1  of  ABD .
+C           In addition, the first  ML  rows in  ABD  are used for
+C           elements generated during the triangularization.
+C           The total number of rows needed in  ABD  is  2*ML+MU+1 .
+C           The  ML+MU by ML+MU  upper left triangle and the
+C           ML by ML  lower right triangle are not referenced.
+C
+C***REFERENCES  J. J. Dongarra, J. R. Bunch, C. B. Moler, and G. W.
+C                 Stewart, LINPACK Users' Guide, SIAM, 1979.
+C***ROUTINES CALLED  DAXPY, DSCAL, IDAMAX
+C***REVISION HISTORY  (YYMMDD)
+C   780814  DATE WRITTEN
+C   890531  Changed all specific intrinsics to generic.  (WRB)
+C   890831  Modified array declarations.  (WRB)
+C   890831  REVISION DATE from Version 3.2
+C   891214  Prologue converted to Version 4.0 format.  (BAB)
+C   900326  Removed duplicate information from DESCRIPTION section.
+C           (WRB)
+C   920501  Reformatted the REFERENCES section.  (WRB)
+C***END PROLOGUE  DGBFA
+      INTEGER LDA,N,ML,MU,IPVT(*),INFO
+      DOUBLE PRECISION ABD(LDA,*)
+C
+      DOUBLE PRECISION T
+      INTEGER I,IDAMAX,I0,J,JU,JZ,J0,J1,K,KP1,L,LM,M,MM,NM1
+C
+C***FIRST EXECUTABLE STATEMENT  DGBFA
+      M = ML + MU + 1
+      INFO = 0
+C
+C     ZERO INITIAL FILL-IN COLUMNS
+C
+      J0 = MU + 2
+      J1 = MIN(N,M) - 1
+      IF (J1 .LT. J0) GO TO 30
+      DO 20 JZ = J0, J1
+         I0 = M + 1 - JZ
+         DO 10 I = I0, ML
+            ABD(I,JZ) = 0.0D0
+   10    CONTINUE
+   20 CONTINUE
+   30 CONTINUE
+      JZ = J1
+      JU = 0
+C
+C     GAUSSIAN ELIMINATION WITH PARTIAL PIVOTING
+C
+      NM1 = N - 1
+      IF (NM1 .LT. 1) GO TO 130
+      DO 120 K = 1, NM1
+         KP1 = K + 1
+C
+C        ZERO NEXT FILL-IN COLUMN
+C
+         JZ = JZ + 1
+         IF (JZ .GT. N) GO TO 50
+         IF (ML .LT. 1) GO TO 50
+            DO 40 I = 1, ML
+               ABD(I,JZ) = 0.0D0
+   40       CONTINUE
+   50    CONTINUE
+C
+C        FIND L = PIVOT INDEX
+C
+         LM = MIN(ML,N-K)
+         L = IDAMAX(LM+1,ABD(M,K),1) + M - 1
+         IPVT(K) = L + K - M
+C
+C        ZERO PIVOT IMPLIES THIS COLUMN ALREADY TRIANGULARIZED
+C
+         IF (ABD(L,K) .EQ. 0.0D0) GO TO 100
+C
+C           INTERCHANGE IF NECESSARY
+C
+            IF (L .EQ. M) GO TO 60
+               T = ABD(L,K)
+               ABD(L,K) = ABD(M,K)
+               ABD(M,K) = T
+   60       CONTINUE
+C
+C           COMPUTE MULTIPLIERS
+C
+            T = -1.0D0/ABD(M,K)
+            CALL DSCAL(LM,T,ABD(M+1,K),1)
+C
+C           ROW ELIMINATION WITH COLUMN INDEXING
+C
+            JU = MIN(MAX(JU,MU+IPVT(K)),N)
+            MM = M
+            IF (JU .LT. KP1) GO TO 90
+            DO 80 J = KP1, JU
+               L = L - 1
+               MM = MM - 1
+               T = ABD(L,J)
+               IF (L .EQ. MM) GO TO 70
+                  ABD(L,J) = ABD(MM,J)
+                  ABD(MM,J) = T
+   70          CONTINUE
+               CALL DAXPY(LM,T,ABD(M+1,K),1,ABD(MM+1,J),1)
+   80       CONTINUE
+   90       CONTINUE
+         GO TO 110
+  100    CONTINUE
+            INFO = K
+  110    CONTINUE
+  120 CONTINUE
+  130 CONTINUE
+      IPVT(N) = N
+      IF (ABD(M,N) .EQ. 0.0D0) INFO = N
+      RETURN
+      END
+*DECK DGBSL
+      SUBROUTINE DGBSL (ABD, LDA, N, ML, MU, IPVT, B, JOB)
+C***BEGIN PROLOGUE  DGBSL
+C***PURPOSE  Solve the real band system A*X=B or TRANS(A)*X=B using
+C            the factors computed by DGBCO or DGBFA.
+C***CATEGORY  D2A2
+C***TYPE      DOUBLE PRECISION (SGBSL-S, DGBSL-D, CGBSL-C)
+C***KEYWORDS  BANDED, LINEAR ALGEBRA, LINPACK, MATRIX, SOLVE
+C***AUTHOR  Moler, C. B., (U. of New Mexico)
+C***DESCRIPTION
+C
+C     DGBSL solves the double precision band system
+C     A * X = B  or  TRANS(A) * X = B
+C     using the factors computed by DGBCO or DGBFA.
+C
+C     On Entry
+C
+C        ABD     DOUBLE PRECISION(LDA, N)
+C                the output from DGBCO or DGBFA.
+C
+C        LDA     INTEGER
+C                the leading dimension of the array  ABD .
+C
+C        N       INTEGER
+C                the order of the original matrix.
+C
+C        ML      INTEGER
+C                number of diagonals below the main diagonal.
+C
+C        MU      INTEGER
+C                number of diagonals above the main diagonal.
+C
+C        IPVT    INTEGER(N)
+C                the pivot vector from DGBCO or DGBFA.
+C
+C        B       DOUBLE PRECISION(N)
+C                the right hand side vector.
+C
+C        JOB     INTEGER
+C                = 0         to solve  A*X = B ,
+C                = nonzero   to solve  TRANS(A)*X = B , where
+C                            TRANS(A)  is the transpose.
+C
+C     On Return
+C
+C        B       the solution vector  X .
+C
+C     Error Condition
+C
+C        A division by zero will occur if the input factor contains a
+C        zero on the diagonal.  Technically this indicates singularity
+C        but it is often caused by improper arguments or improper
+C        setting of LDA .  It will not occur if the subroutines are
+C        called correctly and if DGBCO has set RCOND .GT. 0.0
+C        or DGBFA has set INFO .EQ. 0 .
+C
+C     To compute  INVERSE(A) * C  where  C  is a matrix
+C     with  P  columns
+C           CALL DGBCO(ABD,LDA,N,ML,MU,IPVT,RCOND,Z)
+C           IF (RCOND is too small) GO TO ...
+C           DO 10 J = 1, P
+C              CALL DGBSL(ABD,LDA,N,ML,MU,IPVT,C(1,J),0)
+C        10 CONTINUE
+C
+C***REFERENCES  J. J. Dongarra, J. R. Bunch, C. B. Moler, and G. W.
+C                 Stewart, LINPACK Users' Guide, SIAM, 1979.
+C***ROUTINES CALLED  DAXPY, DDOT
+C***REVISION HISTORY  (YYMMDD)
+C   780814  DATE WRITTEN
+C   890531  Changed all specific intrinsics to generic.  (WRB)
+C   890831  Modified array declarations.  (WRB)
+C   890831  REVISION DATE from Version 3.2
+C   891214  Prologue converted to Version 4.0 format.  (BAB)
+C   900326  Removed duplicate information from DESCRIPTION section.
+C           (WRB)
+C   920501  Reformatted the REFERENCES section.  (WRB)
+C***END PROLOGUE  DGBSL
+      INTEGER LDA,N,ML,MU,IPVT(*),JOB
+      DOUBLE PRECISION ABD(LDA,*),B(*)
+C
+      DOUBLE PRECISION DDOT,T
+      INTEGER K,KB,L,LA,LB,LM,M,NM1
+C***FIRST EXECUTABLE STATEMENT  DGBSL
+      M = MU + ML + 1
+      NM1 = N - 1
+      IF (JOB .NE. 0) GO TO 50
+C
+C        JOB = 0 , SOLVE  A * X = B
+C        FIRST SOLVE L*Y = B
+C
+         IF (ML .EQ. 0) GO TO 30
+         IF (NM1 .LT. 1) GO TO 30
+            DO 20 K = 1, NM1
+               LM = MIN(ML,N-K)
+               L = IPVT(K)
+               T = B(L)
+               IF (L .EQ. K) GO TO 10
+                  B(L) = B(K)
+                  B(K) = T
+   10          CONTINUE
+               CALL DAXPY(LM,T,ABD(M+1,K),1,B(K+1),1)
+   20       CONTINUE
+   30    CONTINUE
+C
+C        NOW SOLVE  U*X = Y
+C
+         DO 40 KB = 1, N
+            K = N + 1 - KB
+            B(K) = B(K)/ABD(M,K)
+            LM = MIN(K,M) - 1
+            LA = M - LM
+            LB = K - LM
+            T = -B(K)
+            CALL DAXPY(LM,T,ABD(LA,K),1,B(LB),1)
+   40    CONTINUE
+      GO TO 100
+   50 CONTINUE
+C
+C        JOB = NONZERO, SOLVE  TRANS(A) * X = B
+C        FIRST SOLVE  TRANS(U)*Y = B
+C
+         DO 60 K = 1, N
+            LM = MIN(K,M) - 1
+            LA = M - LM
+            LB = K - LM
+            T = DDOT(LM,ABD(LA,K),1,B(LB),1)
+            B(K) = (B(K) - T)/ABD(M,K)
+   60    CONTINUE
+C
+C        NOW SOLVE TRANS(L)*X = Y
+C
+         IF (ML .EQ. 0) GO TO 90
+         IF (NM1 .LT. 1) GO TO 90
+            DO 80 KB = 1, NM1
+               K = N - KB
+               LM = MIN(ML,N-K)
+               B(K) = B(K) + DDOT(LM,ABD(M+1,K),1,B(K+1),1)
+               L = IPVT(K)
+               IF (L .EQ. K) GO TO 70
+                  T = B(L)
+                  B(L) = B(K)
+                  B(K) = T
+   70          CONTINUE
+   80       CONTINUE
+   90    CONTINUE
+  100 CONTINUE
+      RETURN
+      END
